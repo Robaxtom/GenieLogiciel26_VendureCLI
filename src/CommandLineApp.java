bce8294461ae8e918721fa6cbdcafb177314ac92
Robin Saint-Ghislain <robin.saintghislain@icloud.com>
2026-05-01 18:06:44 +0200

    Implemented Picocli and integrated in CommandLineApp

--- src/CommandLineApp.java
+++ src/CommandLineApp.java
@@ -1,5 +1,17 @@
-public class CommandLineApp {
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "vendure-cli", mixinStandardHelpOptions = true)
+public class CommandLineApp implements Runnable{
     private CommandStrategy commandStrategy;
+
+    @CommandLine.Option(names = {"-u", "--url"})
+    private String url;
+
+    @CommandLine.Option(names = {"-f", "--format"}, defaultValue = "table")
+    private String format;
+
+    @CommandLine.Parameters(index = "0")
+    private String commandName;
 
     public static void main(String[] args) {
     }
@@ -13,5 +25,9 @@ public class CommandLineApp {
         return null;
     }
 
+    @Override
+    public void run() {
+
+    }
 }
 
